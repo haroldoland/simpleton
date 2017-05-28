a800895bfb261254064f57bcc63eb8c481178cee
Harold <hvalenzuela@gmail.com>
2017-05-28 14:47:05 -0700

    Update dbcheck.php
    Using mysqli for use with PHP7.

--- dbcheck.php
+++ dbcheck.php
@@ -1,34 +1,32 @@
 <?php
 // Azure MySQl In App Connectivity Test
 
-$connectstr_dbhost = '';
-$connectstr_dbname = '';
-$connectstr_dbusername = '';
-$connectstr_dbpassword = '';
-
-foreach ($_SERVER as $key => $value) {
-    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
-        continue;
-    }
-    
-    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+$AzureConnString = $_SERVER['MYSQLCONNSTR_localdb'];
+$AzureConnStringPieces = explode(";", $AzureConnString);
+foreach ($AzureConnStringPieces as $piece) {
+    //Parse each piece of the string creating $Data_Source (Server:Port), $User_Id (Username), $Password, $Database (DBName)
+    //IE. Data Source=127.0.0.1:45678 --> $Data_Source = "127.0.0.1:45678"
+    parse_str($piece);
 }
 
-$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
+// Separate host from port in Data_Source
+$source = explode(":", $Data_Source);
+$Host = $source[0];
+$Port = $source[1];
+
+// Create connection
+$link = new mysqli($Host, $User_Id, $Password, $Database, $Port);
 
 if (!$link) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    echo "Debugging errno: " . $clink->connect_error . PHP_EOL;
+    echo "Debugging error: " . $clink->connect_error . PHP_EOL;
     exit;
 }
 
 echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+echo "Host information: " . $link->host_info . PHP_EOL;
 
-mysqli_close($link);
+$link->close();
 
 ?>
